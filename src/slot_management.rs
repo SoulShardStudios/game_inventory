6a552b3f208c15a54a062d3c40b395cb2487e5dd
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-07-21 17:07:51 -0400

    refactor: Unindent a bunch of stuff by doing let x = match

--- src/slot_management.rs
+++ src/slot_management.rs
@@ -17,30 +17,29 @@ pub fn combine_stack<'a, II>(items: (Option<II>, Option<II>)) -> (Option<II>, Op
 where
     II: traits::IItemInstance<'a> + Copy + 'a,
 {
-    return match items {
-        (Some(c), Some(o)) => {
-            if c.item().name() != o.item().name() {
-                return swap(items);
-            }
-            if !c.item().stackable() {
-                return swap(items);
-            }
-            let stack_size = c.item().max_quant();
-            if c.quant() >= stack_size || o.quant() >= stack_size {
-                return swap(items);
-            }
-            let combined = c.quant() + o.quant();
-            if combined < stack_size {
-                return (None, Some(II::new(c.item(), combined)));
-            }
-            let left_over = combined - stack_size;
-            return (
-                Some(II::new(c.item(), left_over)),
-                Some(II::new(c.item(), stack_size)),
-            );
-        }
-        _ => swap(items),
+    let (c, o) = match items {
+        (Some(c), Some(o)) => (c, o),
+        _ => return swap(items),
     };
+    if c.item().name() != o.item().name() {
+        return swap(items);
+    }
+    if !c.item().stackable() {
+        return swap(items);
+    }
+    let stack_size = c.item().max_quant();
+    if c.quant() >= stack_size || o.quant() >= stack_size {
+        return swap(items);
+    }
+    let combined = c.quant() + o.quant();
+    if combined < stack_size {
+        return (None, Some(II::new(c.item(), combined)));
+    }
+    let left_over = combined - stack_size;
+    return (
+        Some(II::new(c.item(), left_over)),
+        Some(II::new(c.item(), stack_size)),
+    );
 }
 
 /// Splits a stack of items into two. Tries to split `items.0` and put the second half into `items.1`
@@ -51,36 +50,34 @@ pub fn half_stack_split<'a, II>(items: (Option<II>, Option<II>)) -> (Option<II>,
 where
     II: traits::IItemInstance<'a> + Copy + 'a,
 {
-    return match items.0 {
-        Some(c) => {
-            if !c.item().stackable() {
-                return swap(items);
-            }
-            if match items.1 {
-                Some(o) => c.item().name() != o.item().name(),
-                None => false,
-            } {
-                return swap(items);
-            }
-            if c.quant() < 2 {
-                return swap(items);
-            }
-            let other_quant = match items.1 {
-                Some(o) => o.quant(),
-                None => 0,
-            };
-
-            let half_stack = c.quant() / 2;
-            return (
-                Some(II::new(c.item(), half_stack)),
-                Some(II::new(
-                    c.item(),
-                    other_quant + half_stack + (c.quant() % 2),
-                )),
-            );
-        }
-        None => swap(items),
+    let c = match items.0 {
+        Some(c) => c,
+        None => return swap(items),
     };
+    if !c.item().stackable() {
+        return swap(items);
+    }
+    if match items.1 {
+        Some(o) => c.item().name() != o.item().name(),
+        None => false,
+    } {
+        return swap(items);
+    }
+    if c.quant() < 2 {
+        return swap(items);
+    }
+    let other_quant = match items.1 {
+        Some(o) => o.quant(),
+        None => 0,
+    };
+    let half_stack = c.quant() / 2;
+    return (
+        Some(II::new(c.item(), half_stack)),
+        Some(II::new(
+            c.item(),
+            other_quant + half_stack + (c.quant() % 2),
+        )),
+    );
 }
 
 /// Removes a single item from a stack. Tries to take a single item from `items.0` and put it into `items.1`.
@@ -91,38 +88,36 @@ pub fn remove_from_stack<'a, II>(items: (Option<II>, Option<II>)) -> (Option<II>
 where
     II: traits::IItemInstance<'a> + Copy + 'a,
 {
-    return match items.0 {
-        Some(c) => {
-            if !c.item().stackable() {
-                return swap(items);
-            }
-            match items.1 {
-                Some(o) => {
-                    if o.item().name() != c.item().name() {
-                        return swap(items);
-                    }
-                    if o.quant() == o.item().max_quant() {
-                        return swap(items);
-                    }
-                    if c.quant() < 2 {
-                        return (None, Some(II::new(o.item(), o.quant() + 1)));
-                    }
-                    return (
-                        Some(II::new(c.item(), c.quant() - 1)),
-                        Some(II::new(o.item(), o.quant() + 1)),
-                    );
-                }
-                None => {
-                    if c.quant() < 2 {
-                        return (None, Some(II::new(c.item(), 1)));
-                    }
-                    return (
-                        Some(II::new(c.item(), c.quant() - 1)),
-                        Some(II::new(c.item(), 1)),
-                    );
-                }
-            }
-        }
-        None => swap(items),
+    let c = match items.0 {
+        Some(c) => c,
+        None => return swap(items),
     };
+    if !c.item().stackable() {
+        return swap(items);
+    }
+    let o = match items.1 {
+        Some(o) => o,
+        None => {
+            if c.quant() < 2 {
+                return (None, Some(II::new(c.item(), 1)));
+            }
+            return (
+                Some(II::new(c.item(), c.quant() - 1)),
+                Some(II::new(c.item(), 1)),
+            );
+        }
+    };
+    if o.item().name() != c.item().name() {
+        return swap(items);
+    }
+    if o.quant() == o.item().max_quant() {
+        return swap(items);
+    }
+    if c.quant() < 2 {
+        return (None, Some(II::new(o.item(), o.quant() + 1)));
+    }
+    return (
+        Some(II::new(c.item(), c.quant() - 1)),
+        Some(II::new(o.item(), o.quant() + 1)),
+    );
 }
