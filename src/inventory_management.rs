5caf8905a226a9b9585657018bee7a6196592767
Blair Hines <Lubba64@gmail.com>
2024-10-29 13:35:01 -0400

    feat: can_add_item

--- src/inventory_management.rs
+++ src/inventory_management.rs
@@ -191,34 +191,43 @@ pub fn add_to_inventory<Id: Eq, I: Item<Id = Id>, II: ItemInstance<I> + Clone, S
     if inventory.capacity() == 0 {
         return None;
     }
-    fn try_add_to_slot<Id: Eq, I: Item<Id = Id>, II: ItemInstance<I> + Clone, S: Slot<I, II>>(
-        other: Option<II>,
-        slot: &mut S,
-    ) -> Option<II> {
-        let c = match &other {
-            None => return None,
-            Some(c) => c,
-        };
 
-        let s = match slot.item_instance() {
-            None => {
-                slot.set_item_instance(&other);
-                return None;
-            }
-            Some(s) => s,
-        };
+    return inventory
+        .iter_mut()
+        .fold(Some(other), add_to_inventory_try_add_to_slot);
+}
 
-        if s.item().id() != c.item().id() {
-            return other;
+fn add_to_inventory_try_add_to_slot<
+    Id: Eq,
+    I: Item<Id = Id>,
+    II: ItemInstance<I> + Clone,
+    S: Slot<I, II>,
+>(
+    other: Option<II>,
+    slot: &mut S,
+) -> Option<II> {
+    let c = match &other {
+        None => return None,
+        Some(c) => c,
+    };
+
+    let s = match slot.item_instance() {
+        None => {
+            slot.set_item_instance(&other);
+            return None;
         }
-        if s.quant() == s.item().max_quant() {
-            return other;
-        }
-        let res = unwrap_items_res(combine_stack((slot.item_instance(), Some(c.clone()))));
-        slot.set_item_instance(&res.1);
-        return res.0;
+        Some(s) => s,
+    };
+
+    if s.item().id() != c.item().id() {
+        return other;
     }
-    return inventory.iter_mut().fold(Some(other), try_add_to_slot);
+    if s.quant() == s.item().max_quant() {
+        return other;
+    }
+    let res = unwrap_items_res(combine_stack((slot.item_instance(), Some(c.clone()))));
+    slot.set_item_instance(&res.1);
+    return res.0;
 }
 
 /// Attempts to remove an item from the given inventory.
@@ -292,38 +301,185 @@ pub fn remove_from_inventory<Id: Eq, I: Item<Id = Id>, II: ItemInstance<I>, S: S
     inventory: &mut Vec<S>,
     other: II,
 ) -> Option<II> {
-    fn try_remove<Id: Eq, I: Item<Id = Id>, II: ItemInstance<I>, S: Slot<I, II>>(
-        current: u16,
-        slot: &mut S,
-        other: &II,
-    ) -> u16 {
-        let s = match slot.item_instance() {
-            None => return current,
-            Some(s) => s,
-        };
-        if current == 0 {
-            return 0;
-        }
-        if s.item().id() != other.item().id() {
-            return current;
-        }
-        if !s.item().stackable() {
-            slot.set_item_instance(&None);
-            return current - 1;
-        }
-        if s.quant() <= current {
-            slot.set_item_instance(&None);
-            return current - s.quant();
-        }
-        slot.set_item_instance(&Some(II::new(s.item(), s.quant() - current)));
-        0
-    }
-
     let remaining = inventory.iter_mut().fold(other.quant(), |current, slot| {
-        try_remove(current, slot, &other)
+        remove_from_inventory_try_remove(current, slot, &other)
     });
     if remaining == 0 {
         return None;
     }
     return Some(II::new(other.item(), remaining));
+}
+
+fn remove_from_inventory_try_remove<
+    Id: Eq,
+    I: Item<Id = Id>,
+    II: ItemInstance<I>,
+    S: Slot<I, II>,
+>(
+    current: u16,
+    slot: &mut S,
+    other: &II,
+) -> u16 {
+    let s = match slot.item_instance() {
+        None => return current,
+        Some(s) => s,
+    };
+    if current == 0 {
+        return 0;
+    }
+    if s.item().id() != other.item().id() {
+        return current;
+    }
+    if !s.item().stackable() {
+        slot.set_item_instance(&None);
+        return current - 1;
+    }
+    if s.quant() <= current {
+        slot.set_item_instance(&None);
+        return current - s.quant();
+    }
+    slot.set_item_instance(&Some(II::new(s.item(), s.quant() - current)));
+    0
+}
+
+/// Checks if an item can be added to the given inventory.
+///
+/// This function evaluates whether an item can be added based on whether it is stackable or unstackable.
+/// For stackable items, it checks if there is enough space to add more items to existing stacks to the inventory. For unstackable
+/// items, it checks if there are empty slots in the inventory.
+///
+/// Adding a stackable item to an inventory with space
+/// ```
+/// # use std::sync::Arc;
+/// # use game_inventory::samples::{DefaultSlot, DefaultItemInstance, TORCH, TORCH_INST};
+/// # use game_inventory::traits::{Slot, ItemInstance, Item};
+/// # use game_inventory::helpers::can_add_item_to_inventory;
+/// let inventory = vec![
+///     DefaultSlot::new(None),
+///     DefaultSlot::new(TORCH_INST.clone()),
+///     DefaultSlot::new(None),
+/// ];
+/// let can_add = can_add_item_to_inventory(&inventory, DefaultItemInstance::new(Arc::new(TORCH.clone()), 10));
+/// assert!(can_add);
+/// ```
+///
+/// Adding an unstackable item to an inventory with space
+/// ```
+/// # use std::sync::Arc;
+/// # use game_inventory::samples::{DefaultSlot, DefaultItemInstance, SWORD, SWORD_INST};
+/// # use game_inventory::traits::{Slot, ItemInstance, Item};
+/// # use game_inventory::helpers::can_add_item_to_inventory;
+/// let inventory = vec![
+///     DefaultSlot::new(SWORD_INST.clone()),
+///     DefaultSlot::new(None),
+/// ];
+/// let can_add = can_add_item_to_inventory(&inventory, DefaultItemInstance::new(Arc::new(SWORD.clone()), 1));
+/// assert!(can_add);
+/// ```
+///
+/// Adding to a full inventory
+/// ```
+/// # use std::sync::Arc;
+/// # use game_inventory::samples::{DefaultSlot, DefaultItemInstance, TORCH_FULL_STACK_INST, SWORD_INST, TORCH};
+/// # use game_inventory::traits::{Slot, ItemInstance, Item};
+/// # use game_inventory::helpers::can_add_item_to_inventory;
+/// let inventory = vec![
+///     DefaultSlot::new(TORCH_FULL_STACK_INST.clone()),
+///     DefaultSlot::new(SWORD_INST.clone()),
+/// ];
+/// let can_add = can_add_item_to_inventory(&inventory, DefaultItemInstance::new(Arc::new(TORCH.clone()), 1));
+/// assert!(!can_add);
+/// ```
+///
+/// Adding a stackable item to an inventory with partial stacks
+/// ```
+/// # use std::sync::Arc;
+/// # use game_inventory::samples::{DefaultSlot, DefaultItemInstance, TORCH, TORCH_INST, JUNK};
+/// # use game_inventory::traits::{Slot, ItemInstance, Item};
+/// # use game_inventory::helpers::can_add_item_to_inventory;
+/// let inventory = vec![
+///     DefaultSlot::new(Some(DefaultItemInstance::new(Arc::new(TORCH.clone()), 50))),
+///     DefaultSlot::new(Some(DefaultItemInstance::new(Arc::new(JUNK.clone()), 30))),
+/// ];
+/// let can_add = can_add_item_to_inventory(&inventory, DefaultItemInstance::new(Arc::new(TORCH.clone()), 50));
+/// assert!(can_add);
+/// ```
+///
+/// Attempting to add a stackable item that exceeds the maximum quantity
+/// ```
+/// # use std::sync::Arc;
+/// # use game_inventory::samples::{DefaultSlot, DefaultItemInstance, TORCH};
+/// # use game_inventory::traits::{Slot, ItemInstance, Item};
+/// # use game_inventory::helpers::can_add_item_to_inventory;
+/// let inventory = vec![
+///     DefaultSlot::new(Some(DefaultItemInstance::new(Arc::new(TORCH.clone()), 70))),
+/// ];
+/// let can_add = can_add_item_to_inventory(&inventory, DefaultItemInstance::new(Arc::new(TORCH.clone()), 31));
+/// assert!(!can_add);
+/// ```
+pub fn can_add_item_to_inventory<Id: Eq, I: Item<Id = Id>, II: ItemInstance<I>, S: Slot<I, II>>(
+    inventory: &Vec<S>,
+    other: II,
+) -> bool {
+    if !other.item().stackable() {
+        return can_add_unstackable_item_to_inventory(inventory);
+    }
+    return can_add_stackable_item_to_inventory(inventory, other);
+}
+
+fn can_add_unstackable_item_to_inventory<
+    Id: Eq,
+    I: Item<Id = Id>,
+    II: ItemInstance<I>,
+    S: Slot<I, II>,
+>(
+    inventory: &Vec<S>,
+) -> bool {
+    inventory.iter().any(|slot| slot.item_instance().is_none())
+}
+
+fn can_add_stackable_item_to_inventory<
+    Id: Eq,
+    I: Item<Id = Id>,
+    II: ItemInstance<I>,
+    S: Slot<I, II>,
+>(
+    inventory: &Vec<S>,
+    mut other: II,
+) -> bool {
+    let max_stack = other.item().max_quant();
+    if other.quant() == max_stack {
+        return can_add_unstackable_item_to_inventory(inventory);
+    }
+
+    for slot in inventory {
+        let item = match slot.item_instance() {
+            None => {
+                return true;
+            }
+            Some(x) => x,
+        };
+
+        if item.item().id() == other.item().id() {
+            if item.quant() == max_stack {
+                continue;
+            }
+            if item.quant() + other.quant() <= max_stack {
+                return true;
+            }
+
+            let space_available = max_stack - item.quant();
+            other = II::new(other.item(), space_available);
+        }
+    }
+
+    if other.quant() == 0 {
+        return true;
+    }
+
+    if other.quant() > 0 {
+        return can_add_unstackable_item_to_inventory(inventory);
+    }
+
+    false
 }
